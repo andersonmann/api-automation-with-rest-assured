594df3481a806a0a7ae356a76fc8890067a39fba
Anderson Mann <anderson.mann@br.experian.com>
2023-02-08 18:12:18 -0300

    refactor: code  refactor

--- src/main/java/br/rs/ammann/rest/test/DeleteSimulationTest.java
+++ src/main/java/br/rs/ammann/rest/test/DeleteSimulationTest.java
@@ -3,13 +3,33 @@ package br.rs.ammann.rest.test;
 import br.rs.ammann.rest.core.BaseTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertTrue;
 
 public class DeleteSimulationTest extends BaseTest {
     @Test(priority = 1, description = "Deleta a simulação desejada", groups = {"smoke.test"})
     public void deleteSimulation(){
-        given()
-        .when()
+//        ArrayList<String> simulations =
+//                given()
+//                        .when()
+//                        .get("simulacoes")
+//                        .then()
+//                        .statusCode(200)
+//                        .extract().path("nome.findAll{it != null}")
+//                ;
+//        assertTrue(!simulations.isEmpty());
+                ArrayList<String> simulations =
+                given()
+                .when()
+                        .get("simulacoes")
+                .then()
+                        .statusCode(200)
+                        .extract().path("id.findAll{it != null}")
+                        ;
+                given()
+                        .when()
                 .delete("simulacoes/13")
         .then()
                 .log().all()
