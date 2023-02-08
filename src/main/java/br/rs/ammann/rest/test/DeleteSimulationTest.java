47ee68e49b19ce33837881842a2aa39de2070fe8
Anderson Mann <anderson.mann@br.experian.com>
2023-02-08 17:59:38 -0300

    refactor: code  refactor

--- src/main/java/br/rs/ammann/rest/test/DeleteSimulationTest.java
+++ src/main/java/br/rs/ammann/rest/test/DeleteSimulationTest.java
@@ -1,16 +1,16 @@
-package br.rs.ammann.rest.tasks;
+package br.rs.ammann.rest.test;
 
 import br.rs.ammann.rest.core.BaseTest;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class DeleteSimulationTest extends BaseTest {
-    @Test
+    @Test(priority = 1, description = "Deleta a simulação desejada", groups = {"smoke.test"})
     public void deleteSimulation(){
         given()
         .when()
-                .delete("simulacoes/63")
+                .delete("simulacoes/13")
         .then()
                 .log().all()
                 .statusCode(200)
