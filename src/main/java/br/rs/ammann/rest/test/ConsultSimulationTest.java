ee78c50dadf7dc773965006c59cddc7692f6ba78
Anderson Mann <anderson.mann@br.experian.com>
2023-02-08 16:02:46 -0300

    refactor: created test class ConsultSimulationTest

--- src/main/java/br/rs/ammann/rest/test/ConsultSimulationTest.java
+++ src/main/java/br/rs/ammann/rest/test/ConsultSimulationTest.java
@@ -1,17 +1,17 @@
-package br.rs.ammann.rest.tasks;
+package br.rs.ammann.rest.test;
 
 import br.rs.ammann.rest.core.BaseTest;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.is;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class ConsultSimulationTest extends BaseTest {
-    @Test
+    @Test(priority = 1, description = "Realiza a consulta de uma simulação", groups = {"smoke.test"})
     public void consultSimulationTest(){
         given()
         .when()
@@ -22,8 +22,7 @@ public class ConsultSimulationTest extends BaseTest {
                 .body("cpf", is("66414919004"))
         ;
     }
-
-    @Test
+    @Test(priority = 2, description = "Consulta um CPF inexistente", groups = {"smoke.test"})
     public void consultCpfWithOutSimulationTest(){
         given()
         .when()
@@ -32,5 +31,30 @@ public class ConsultSimulationTest extends BaseTest {
                 .statusCode(404)
                 .body("mensagem", is("CPF 66414919005 não encontrado"))
         ;
+    }
+    @Test(priority = 3, description = "Recebe uma lista de simulações e verifica a existência de um nome específico", groups = {"smoke.test"})
+    public void consultSimulationByName(){
+        ArrayList<String> names =
+                given()
+                .when()
+                        .get("simulacoes")
+                .then()
+                        .statusCode(200)
+                        .extract().path("nome.findAll{it.startsWith('Deltrano')}")
+                ;
+        assertEquals(1, names.size());
+        assertEquals("Deltrano", names.get(0));
+    }
+    @Test(priority = 4, description = "Verifica se existe alguma simulação cadastrada", groups = {"smoke.test"})
+    public void verifyIfListOfSimulationIsEmpity(){
+        ArrayList<String> simulations =
+                given()
+                .when()
+                        .get("simulacoes")
+                .then()
+                        .statusCode(200)
+                        .extract().path("nome.findAll{it != null}")
+                ;
+        assertTrue(!simulations.isEmpty());
     }
 }
