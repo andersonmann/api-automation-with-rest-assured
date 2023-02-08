a302ebf258520ce582a631f613540f7de54b4fe3
Anderson Mann <anderson.mann@br.experian.com>
2023-02-08 16:40:48 -0300

    refactor: code format

--- src/main/java/br/rs/ammann/rest/test/UpdateSimulationTest.java
+++ src/main/java/br/rs/ammann/rest/test/UpdateSimulationTest.java
@@ -5,6 +5,10 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+/**
+ * @author anderson.mann
+ *
+ */
 
 public class UpdateSimulationTest extends BaseTest {
 
